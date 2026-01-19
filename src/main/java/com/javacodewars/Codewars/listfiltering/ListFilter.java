de94c7985a355f662d19e0cc64fc8660951481e0
Matthew <m.mccoach88@gmail.com>
2026-01-19 20:05:12 +0000

    Clean up.

--- src/main/java/com/javacodewars/Codewars/listfiltering/ListFilter.java
+++ src/main/java/com/javacodewars/Codewars/listfiltering/ListFilter.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 public class ListFilter {
     public static List<Object> filterList(final List<Object> list) {
-        //for each item in list if is a number add to list
-        //return list
-        return list.stream().filter(item -> item instanceof Integer).collect(Collectors.toList());
+        return list.
+                stream()
+                .filter(item -> item instanceof Integer)
+                .collect(Collectors.toList());
     }
 }
